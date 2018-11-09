8a3729c4dca72716a8ffcbd87d5165f4d3e3ed2b
wshuai1234 <wshuai951223@gmail.com>
2018-11-09 00:18:39 -0500

    second commit

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://104.196.151.210:9200"
+	ES_URL = "http://35.231.187.146:9200"
 )
 
 func main() {
