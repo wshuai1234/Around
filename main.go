d09633969a47f2fca29cad7200fcc097294ee50b
wshuai1234 <wshuai951223@gmail.com>
2018-11-09 21:45:41 -0500

    third commit

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"reflect"
 	"strconv"
+	"strings"
 
+	"cloud.google.com/go/storage"
 	"github.com/pborman/uuid"
 	elastic "gopkg.in/olivere/elastic.v3"
 )
@@ -17,15 +21,17 @@ type Location struct {
 	Lon float64 `json:"lon"`
 }
 type Post struct {
+	Url      string   `json:"url"`
 	User     string   `json:"user"`
 	Message  string   `json:"message"`
 	Location Location `json:"location"`
 }
 
 const (
-	INDEX    = "around"
-	TYPE     = "post"
-	DISTANCE = "200km"
+	BUCKET_NAME = "post-images-221904"
+	INDEX       = "around"
+	TYPE        = "post"
+	DISTANCE    = "200km"
 	// Needs to update
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
@@ -73,18 +79,75 @@ func main() {
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one post request.")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	// form data max 32M
+	r.ParseMultipartForm(32 << 20)
+	//parse from data
+	fmt.Printf("Received one post request %s\n", r.FormValue("message"))
+	lat, _ := strconv.ParseFloat(r.FormValue("lat"), 64)
+	lon, _ := strconv.ParseFloat(r.FormValue("lon"), 64)
 
-	decoder := json.NewDecoder(r.Body)
-	var p Post
-	if err := decoder.Decode(&p); err != nil {
+	p := &Post{
+		User:    "1111",
+		Message: r.FormValue("message"),
+		Location: Location{
+			Lat: lat,
+			Lon: lon,
+		},
+	}
+	id := uuid.New()
+
+	file, _, err := r.FormFile("image")
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not setup %v\n", err)
 		panic(err)
 	}
+	defer file.Close()
+	// context  , read credential, then you can read data
+	ctx := context.Background()
 
-	fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	_, attrs, err := saveToGCS(ctx, file, BUCKET_NAME, id)
+	if err != nil {
+		http.Error(w, "GCS is not setup", http.StatusInternalServerError)
+		fmt.Printf("GCS is not set up %v\n", err)
+	}
 
-	id := uuid.New()
+	p.Url = attrs.MediaLink
+
 	// save to ES
-	savetoES(&p, id)
+	savetoES(p, id)
+
+}
+func saveToGCS(ctx context.Context, r io.Reader, bucketName, name string) (*storage.ObjectHandle, *storage.ObjectAttrs, error) {
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	// now we have got the bucket handler
+	// now see if exists
+	bucket := client.Bucket(bucketName)
+	if _, err := bucket.Attrs(ctx); err != nil {
+		return nil, nil, err
+	}
+	obj := bucket.Object(name)
+	wc := obj.NewWriter(ctx)
+	if _, err = io.Copy(wc, r); err != nil {
+		return nil, nil, err
+	}
+	if err := wc.Close(); err != nil {
+		return nil, nil, err
+	}
+	// set all can visit file
+	if err := obj.ACL().Set(ctx, storage.AllUsers, storage.RoleReader); err != nil {
+		return nil, nil, err
+	}
+	// get url
+	attrs, err := obj.Attrs(ctx)
+	fmt.Printf("Post is saved to GCS: %s\n", attrs.MediaLink)
+	return obj, attrs, err
 
 }
 func savetoES(p *Post, id string) {
@@ -142,7 +205,11 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		p := item.(Post) // p = (Post) (item)
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n",
 			p.User, p.Message, p.Location.Lat, p.Location.Lon)
-		ps = append(ps, p)
+		//ps = append(ps, p)
+		if !containsFilteredWords(&p.Message) {
+			ps = append(ps, p)
+		}
+
 	}
 
 	// convert to js string , then return to user
@@ -154,4 +221,17 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
+}
+
+func containsFilteredWords(s *string) bool {
+	filteredWords := []string{
+		"fuck",
+		"100",
+	}
+	for _, word := range filteredWords {
+		if strings.Contains(*s, word) {
+			return true
+		}
+	}
+	return false
 }
